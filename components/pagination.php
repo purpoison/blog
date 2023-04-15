1b61e874eb9ee278a3ffaea8092d3b5d1c2cea8e
Tetiana <shahdi007@gmail.com>
2023-04-15 15:43:47 -0400

    remove paginstion if

--- components/pagination.php
+++ components/pagination.php
@@ -2,7 +2,7 @@
     $dbh = connectToDatabase();
     $total_page = totalPages($dbh, LIMIT);
     echo "<div class='pagination-wrap'>";
-    if (isset($_GET['page']) && intval($_GET['page']) > 1){
+    if (intval($_GET['page']) > 1){
         $page = intval($_GET['page']) - 1;
         echo "<a href='index.php?page={$page}' class='pagination-btn'>Prev</a>";
     }
@@ -11,7 +11,7 @@
         echo "<a href='index.php?page={$btn}' class='pagination-btn' id='{$btn}'>$btn</a>";
     }
 
-    if (isset($_GET['page']) && intval($_GET['page']) < $total_page){
+    if (intval($_GET['page']) < $total_page){
         $page = intval($_GET['page']) + 1;
         echo "<a href='index.php?page={$page}' class='pagination-btn'>Next</a>";
     }
